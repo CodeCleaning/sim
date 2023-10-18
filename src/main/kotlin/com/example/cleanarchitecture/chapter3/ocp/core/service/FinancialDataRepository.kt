0b627db704b1cc90e6733bf00cf07a0d0d351bb0
yhsim98 <2008qwe123@gmail.com>
2023-10-19 00:47:13 +0900

    스터디 정리

--- src/main/kotlin/com/example/cleanarchitecture/chapter3/ocp/core/service/FinancialDataRepository.kt
+++ src/main/kotlin/com/example/cleanarchitecture/chapter3/ocp/core/service/FinancialDataRepository.kt
@@ -2,6 +2,7 @@ package com.example.cleanarchitecture.chapter3.ocp.core.service
 
 import com.example.cleanarchitecture.chapter3.ocp.core.model.FinancialData
 
+// 의존성의 역전을 위한 Interface, 비즈니스 로직을 외부 변경으로부터 보호한다
 interface FinancialDataRepository {
     fun findFinancialDataBySomething() : FinancialData
 }
--- src/main/kotlin/com/example/cleanarchitecture/chapter3/ocp/core/service/FinancialReportService.kt
+++ src/main/kotlin/com/example/cleanarchitecture/chapter3/ocp/core/service/FinancialReportService.kt
@@ -2,6 +2,7 @@ package com.example.cleanarchitecture.chapter3.ocp.core.service
 
 import com.example.cleanarchitecture.chapter3.ocp.core.model.FinancialReport
 
+// 은닉성을 위한 interface 외부로부터 세부적인 구현을 숨긴다
 interface FinancialReportService {
     fun getFinancialReport(): FinancialReport
 }
